5d87f294068dee6cbdd94f03f85876f760520563
Fabian Triana <fabiantriana1072@gmail.com>
2023-04-16 13:03:15 -0500

    Se hace uso de la libreria Pandas para el manejo de archivos CSV, además se utiliza Docker para la app

--- app/charts.py
+++ app/charts.py
@@ -3,13 +3,13 @@ import matplotlib.pyplot as plt
 def generate_bar_chart(labels, values, name):
   fig, ax = plt.subplots()
   ax.bar(labels, values)
-  plt.savefig(f'./images/{name.lower()}_bar.png')
+  plt.savefig(f'./images/{name.lower()}_bar_chart.png')
 
 def generate_pie_chart(labels, values, name):
   fig, ax = plt.subplots()
   ax.pie(values, labels=labels)
   ax.axis('equal')
-  plt.savefig(f'./images/{name.lower()}_pie.png')
+  plt.savefig(f'./images/{name.lower()}_pie_chart.png')
 
 if __name__ == '__main__':
   labels = ['a', 'b', 'c', 'd']
--- app/main.py
+++ app/main.py
@@ -1,13 +1,29 @@
 import utils
 import read_csv
 import charts
-
+import pandas as pd
 
 def run():
+
+  '''
+  
+  data = list(filter(lambda item: item['Continent'] == 'South America', data))
+  countries = list(map(lambda x: x['Country/Territory'], data))
+  percentages = list(map(lambda x: x['World Population Percentage'], data))
+  
+  '''
+  df = pd.read_csv('data.csv')
+  df = df[df['Continent'] == 'North America']
+
+  countries = df['Country/Territory'].values
+  percentages = df['World Population Percentage'].values 
+
+  charts.generate_pie_chart(countries, percentages, 'north_america')
 
   data = read_csv.read_csv('data.csv')
   country = input('Ingrese el país: ')
   result = utils.population_by_country(data, country)
+
   if len(result) > 0:
     country = result[0]
     name_country = country['Country/Territory']
